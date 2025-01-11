e18e9a5a090feedf806d8d1ad234da63200654ad
Manish881-hub <manishbhakti881@gmail.com>
2025-01-11 22:16:17 +0530

    First Commit
    Date 11-01-25

---
+++ server/connectDB.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = async (mongoURI) => {
+  try {
+    const conn = await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
